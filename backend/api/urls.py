5e5e397b0eeaa1c9f8e411b8ec53b6aece7c55dc
PrathamPShetty <prathampshetty99sai@gmail.com>
2024-12-10 12:35:37 +0530

    incomplete

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
-from django.conf import settings
 
 urlpatterns = [
-path('', views.getData),
-path('post/', views.postData),
+    path('', views.getData, name="get_data"),
 ]
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+     'corsheaders',
     "api",
     'rest_framework',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -52,6 +54,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "backend.urls"
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATES = [
     {
